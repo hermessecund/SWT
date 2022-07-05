6b9ba246e3f7a9167450d5b68b5e77ce142e5271
Tarwar <69816954+hermessecund@users.noreply.github.com>
2022-07-05 23:28:29 +0000

    Update MenuItems.jsx

--- src/components/MenuItems.jsx
+++ src/components/MenuItems.jsx
@@ -27,7 +27,7 @@ function MenuItems() {
       <Menu.Item key="/1inch">
         <NavLink to="/1inch">⚡ Exchange</NavLink>
       </Menu.Item>
-        <Menu.Item key="/erc20balance">
+      <Menu.Item key="/erc20balance">
         <NavLink to="/erc20balance">💵 Balances</NavLink>
       </Menu.Item>
       <Menu.Item key="/erc20transfers">
