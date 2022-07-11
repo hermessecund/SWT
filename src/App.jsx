899f555b397838cf0fd9012687da10f520b9f12b
Tarwar <69816954+hermessecund@users.noreply.github.com>
2022-07-11 00:13:46 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -56,9 +56,7 @@ const styles = {
 const App = ({ isServerInfo }) => {
   const { isWeb3Enabled, enableWeb3, isAuthenticated, isWeb3EnableLoading } =
     useMoralis();
-  useMoralis.authenticate({signingMessage:"My custom message"});
-
-  useEffect(() => {
+    useEffect(() => {
     const connectorId = window.localStorage.getItem("connectorId");
     if (isAuthenticated && !isWeb3Enabled && !isWeb3EnableLoading)
       enableWeb3({ provider: connectorId });
